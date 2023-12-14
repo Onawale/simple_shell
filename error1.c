be625cb73bdefddfea880f34d78bfbec8a8f9433
Onawale <onatoyinboonawale@gmail.com>
2023-12-13 19:33:49 -0800

    Redefining the variable of the c file for clarity

--- error1.c
+++ error1.c
@@ -1,24 +1,24 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a string to an integer
+ * _erratoi - It converts a string to an integer
  * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * Return: 0 if there are no numbers in string, converted number otherwise
  *       -1 on error
  */
 int _erratoi(char *s)
 {
-	int i = 0;
+	int u = 0;
 	unsigned long int result = 0;
 
 	if (*s == '+')
 		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
+	for (u = 0;  s[u] != '\0'; u++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[u] >= '0' && s[u] <= '9')
 		{
 			result *= 10;
-			result += (s[i] - '0');
+			result += (s[u] - '0');
 			if (result > INT_MAX)
 				return (-1);
 		}
@@ -29,10 +29,10 @@ int _erratoi(char *s)
 }
 
 /**
- * print_error - prints an error message
- * @info: the parameter & return info struct
- * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
+ * print_error - Prints an error message
+ * @info: The parameter & return info struct
+ * @estr: The string containing specified error type
+ * Return: 0 if there are  no numbers in string, converted number otherwise
  *        -1 on error
  */
 void print_error(info_t *info, char *estr)
@@ -86,7 +86,7 @@ int print_d(int input, int fd)
 }
 
 /**
- * convert_number - converter function, a clone of itoa
+ * convert_number - it converter function, a clone of itoa
  * @num: number
  * @base: base
  * @flags: argument flags
