4659b133045364f23230ed7cf739d324362be278
Onawale <onatoyinboonawale@gmail.com>
2023-12-13 19:56:18 -0800

    Error file redefined

--- error.c
+++ error.c
@@ -1,26 +1,26 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
+ *_eputs - It prints an input string
+ * @str: the string that will printed
  *
  * Return: Nothing
  */
 void _eputs(char *str)
 {
-	int i = 0;
+	int u = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[u] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(str[u]);
+		u++;
 	}
 }
 
 /**
- * _eputchar - writes the character c to stderr
+ * _eputchar - It writes the character c to stderr
  * @c: The character to print
  *
  * Return: On success 1.
@@ -28,21 +28,21 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-	static int i;
+	static int u;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || u >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, u);
+		u = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[u++] = c;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
+ * _putfd - it writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
  *
@@ -51,16 +51,16 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-	static int i;
+	static int u;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || u >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, u);
+		u = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[u++] = c;
 	return (1);
 }
 
@@ -73,13 +73,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+	int u = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		u += _putfd(*str++, fd);
 	}
-	return (i);
+	return (u);
 }
