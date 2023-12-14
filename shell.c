6d953e1e3f810e799245450435bd9d0fdeb89550
Onawale <onatoyinboonawale@gmail.com>
2023-12-13 17:24:07 -0800

    Correction made on shell.c file

--- shell.c
+++ shell.c
@@ -1,26 +1,26 @@
 #include "shell.h"
 
 /**
- * main - entry point
+ * main - The entry point for the program
  * @ac: arg count
  * @av: arg vector
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 if successful or  1 if error occur
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	int fileDescrip = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+		: "=r" (fileDescrip)
+		: "r" (fileDescrip));
 
 	if (ac == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		fileDescrip = open(av[1], O_RDONLY);
+		if (fileDescrip == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
@@ -35,7 +35,7 @@ int main(int ac, char **av)
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		info->readfd = fileDescrip;
 	}
 	populate_env_list(info);
 	read_history(info);
