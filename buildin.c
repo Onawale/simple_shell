3500110d028f85638e6a0924bf6996920ebba581
Onawale <onatoyinboonawale@gmail.com>
2023-12-13 19:25:44 -0800

    File envir.c given a new variable

--- buildin.c
+++ buildin.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
+ * _myexit - it exits the shell
+ * @info: The structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
@@ -30,8 +30,8 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
+ * _mycd - it changes the current directory of the process
+ * @info: The structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
@@ -80,8 +80,8 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
+ * _myhelp - it changes the current directory of the process
+ * @info: The structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
