5d9fbd6cc339784107d3fa57be4e95285a5ed642
Onawale <onatoyinboonawale@gmail.com>
2023-12-13 18:50:51 -0800

    rededining the functions comment attributes

--- buildin1.c
+++ buildin1.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
+ * _myhistory - it displays the history list, one command by line, followed
  *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: The structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
